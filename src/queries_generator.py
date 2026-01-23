d52d3c7707a2cc3e8232b320c8c9a35507b3b25f
cnxls <oles.melinyshyn2007@gmail.com>
2026-01-23 21:03:05 +0100

    Implement query generation functionality with template and brand loading

---
+++ src/queries_generator.py
@@ -0,0 +1,96 @@
+import json
+import logging
+from pathlib import Path
+from typing import Dict, List, Any
+import re
+
+logger = logging.getLogger(__name__)
+
+DATA_DIR = Path(__file__).parent.parent/"data"/"entries"
+TEMPLATES_PATH = DATA_DIR / "query_template.json"
+BRANDS_PATH = DATA_DIR / "brands.json"
+OUTPUT_PATH = DATA_DIR / "queries.json"
+
+
+def load_templates() -> Dict:
+    try:
+        with open(TEMPLATES_PATH, 'r') as file:
+            return json.load(file)
+    
+    except FileNotFoundError:
+        logger.error(f"File {TEMPLATES_PATH} not found.")
+        raise  FileNotFoundError
+    
+    except json.JSONDecodeError:
+        logger.error(f"Error decoding JSON from file {TEMPLATES_PATH}.")
+        raise json.JSONDecodeError
+
+    except PermissionError:
+        logger.error(f"Permission denied reading file: {TEMPLATES_PATH}")
+        raise PermissionError
+
+
+
+def load_brands():
+    competitors = {}
+    try:
+        with open(f'data/entries/brands.json', 'r') as file:
+            brands = json.load(file)
+            target = brands['target']['aliases']
+            for brand in brands['competitors']:
+                competitors[brand['name']] = brand['aliases']
+            return target, competitors 
+    except FileNotFoundError:
+        print("Brands not found.")
+        return [], {}
+
+
+
+def get_use_cases():
+    usecases = load_templates()['placeholders']['use_cases']
+    return usecases
+
+
+def get_placeholders(template):
+    return re.findall(r'\{(\w+)\}',template)
+
+
+def fill_single_template(template, variables):
+    result = template
+    for key in variables:
+        result = result.replace(f'{{{key}}}', variables[key])
+    return result
+
+
+def generate_queries_from_template(template, placeholders):
+    target, competitors = load_brands()
+    needed = get_placeholders(template)
+    use_cases = get_use_cases()
+    
+    base_vars = {
+        "category": placeholders["category"],
+        "target": target[0],
+        "brand1": target[0]
+    }
+    
+    needs_competitor = "brand1" in needed or "competitor" in needed
+    comp_list = list(competitors.keys()) if needs_competitor else [None]
+    case_list = use_cases if "use_case" in needed else [None]
+    
+    results = []
+    for comp in comp_list:
+        for case in case_list:
+            variables = {**base_vars}
+            
+            if comp:
+                variables["brand2"] = comp
+                variables["competitor"] = comp
+            if case:
+                variables["use_case"] = case
+            
+            query = fill_single_template(template, variables)
+            results.append(query)
+    
+    return results
+        
+        
